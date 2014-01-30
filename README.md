b8cc7efe95886eafb8ef8897a580fa689e9cac11
Eugene Hp <eugene.hp2012@gmail.com>
2014-01-30 04:03:30 +0100

    added config

--- README.md
+++ README.md
@@ -2,12 +2,22 @@
 
 AWS SDK based file S3 uploader and Elastic Transcoder Creator
 
+## Installation
+
+`npm install`
+
 ## Usage
 
 Please make sure first you have created the `credentials.json` with all needed data like<br>
 ```
 { "accessKeyId": "YOUR_KEY", "secretAccessKey": "YOUR_SECRET", "region": "us-east-1" }
 ```
+<br>
+And run with the `node index.js`
+
+### FYI
+
+Always make sure you have proper `PresetId` for your availability zone, the same rule for PipelineId and zone.
 
 ## License
 
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var AWS = require('aws-sdk');
+var config = require('./config.json');
 AWS.config.loadFromPath('./credentials.json');
 var elastictranscoder = new AWS.ElasticTranscoder({apiVersion: '2012-09-25'});
 
-var original = "BigBuckBunny_320x180.mp4";
-var filename = new Buffer(original).toString('base64');
+console.log(config);
+
+var original = "demo.flv";
+var filename = original.split('.')[0];
 
 var params = {
 	"Input":{
@@ -20,48 +23,48 @@ var params = {
          "Key":"iphone/"+filename+"-2M",
          "ThumbnailPattern":"iphone/th2M/"+filename+"-{count}",
          "Rotate":"0",
-         "PresetId":"1351620000001-200010",
+         "PresetId":config.HLS['2M'].PresetId,
          "SegmentDuration":"5"
       },
       {
          "Key":"iphone/"+filename+"-1.5M",
          "ThumbnailPattern":"iphone/th1.5M/"+filename+"-{count}",
          "Rotate":"0",
-         "PresetId":"1351620000001-200020",
+         "PresetId":config.HLS['1.5M'].PresetId,
          "SegmentDuration":"5"
       },
       {
          "Key":"iphone/"+filename+"-1M",
          "ThumbnailPattern":"iphone/th1M/"+filename+"-{count}",
          "Rotate":"0",
-         "PresetId":"1351620000001-200030",
+         "PresetId":config.HLS['1M'].PresetId,
          "SegmentDuration":"5"
       },
       {
          "Key":"iphone/"+filename+"-600k",
          "ThumbnailPattern":"iphone/th600k/"+filename+"-{count}",
          "Rotate":"0",
-         "PresetId":"1351620000001-200040",
+         "PresetId":config.HLS['600k'].PresetId,
          "SegmentDuration":"5"
       },
       {
          "Key":"iphone/"+filename+"-400k",
          "ThumbnailPattern":"iphone/th400k/"+filename+"-{count}",
          "Rotate":"0",
-         "PresetId":"1351620000001-200050",
+         "PresetId":config.HLS['400k'].PresetId,
          "SegmentDuration":"5"
       },
       {
-      	"Key":"webm/"+filename,
+      	"Key":"webm/"+filename+".webm",
       	"ThumbnailPattern":"webm/"+filename+"-{count}",
       	"Rotate":"0",
-      	"PresetId":"1390965036083-imf6bl"
+      	"PresetId":config.WebM.PresetId
       },
       {
-      	"Key":"web/"+filename,
+      	"Key":"web/"+filename+".mp4",
       	"ThumbnailPattern":"web/"+filename+"-{count}",
       	"Rotate":"0",
-      	"PresetId":"1351620000001-100070"
+      	"PresetId":config.Web.PresetId
       }
    ],
    "Playlists": [
@@ -77,7 +80,7 @@ var params = {
          ]
       }
    ],
-   "PipelineId":"1390957473527-0bnqqi"
+   "PipelineId":config.PipelineId
 };
 var callback = function(err, data){
 	if(err)
@@ -86,6 +89,6 @@ var callback = function(err, data){
 		console.log(data);
 }
 
-elastictranscoder.createJob(params, callback);
+// elastictranscoder.createJob(params, callback);
 // elastictranscoder.listPipelines({},callback);
 // elastictranscoder.readJob({Id:"1391012229363-mokb38"},callback);
